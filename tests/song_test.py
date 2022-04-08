516aaf16962e998ae7546caf6aaa1ff32cae7223
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-08 12:01:16 +0100

    Add test to check if a song has a name

--- tests/song_test.py
+++ tests/song_test.py
@@ -4,3 +4,6 @@ from src.song import Song
 class TestSong(unittest.TestCase):
     def setUp(self):
         self.song1 = Song("name of song 1")
+
+    def test_song_has_name(self):
+        self.assertEqual("name of song 1", self.song1.name)
