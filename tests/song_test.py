3e049b5c949c581abde44cfbf095fd9880a33c68
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-08 12:04:17 +0100

    Add test to check that a song has a style

--- tests/song_test.py
+++ tests/song_test.py
@@ -3,7 +3,10 @@ from src.song import Song
 
 class TestSong(unittest.TestCase):
     def setUp(self):
-        self.song1 = Song("name of song 1")
+        self.song1 = Song("name of song 1", "rock")
 
     def test_song_has_name(self):
         self.assertEqual("name of song 1", self.song1.name)
+
+    def test_song_has_a_genre(self):
+        self.assertEqual("rock", self.song1.genre)
