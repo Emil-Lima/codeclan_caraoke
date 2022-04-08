5d47441e906c58709d1e8bf26e162aebdcc47e2b
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-08 16:34:01 +0100

    Add favourite song attribute to class Guest

--- src/guest.py
+++ src/guest.py
@@ -1,4 +1,5 @@
 class Guest:
-    def __init__(self, input_name, input_money):
+    def __init__(self, input_name, input_money, input_favourite_song):
         self.name = input_name
         self.money = input_money
+        self.favourite_song = input_favourite_song
