97889205f5acdc63b17d23f9b15e7707c8ad6dcd
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-08 13:38:45 +0100

    Add test to check if karaoke has a list of rooms as an attribute

--- tests/karaoke_test.py
+++ tests/karaoke_test.py
@@ -6,7 +6,14 @@ from src.song import Song
 
 class TestKaraoke(unittest.TestCase):
     def setUp(self):
-        self.karaoke1 = Karaoke("name of karaoke 1")
+        room1 = Room("name of room 1", ["a first list of songs",  "here"], ["a first list of customers", "here"])
+        room2 = Room("name of room 2", ["a second list of songs",  "here"], ["a second list of customers", "here"])
+        self.list_of_rooms = [room1, room2]
+
+        self.karaoke1 = Karaoke("name of karaoke 1", self.list_of_rooms)
     
     def test_karaoke_has_a_name(self):
         self.assertEqual("name of karaoke 1", self.karaoke1.name)
+
+    def test_karaoke_has_list_of_rooms(self):
+        self.assertEqual(self.list_of_rooms, self.karaoke1.rooms)
