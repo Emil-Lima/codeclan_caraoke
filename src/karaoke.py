a452c74c2f6bfcd856591f2e9ad42693f377f207
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-08 14:27:01 +0100

    Add entry_fee attribute to class Karaoke

--- src/karaoke.py
+++ src/karaoke.py
@@ -1,8 +1,9 @@
 class Karaoke:
-    def __init__(self, input_name, input_rooms, input_guests):
+    def __init__(self, input_name, input_rooms, input_guests, input_entry_fee):
         self.name = input_name
         self.rooms = input_rooms
         self.guests = input_guests
+        self.entry_fee = input_entry_fee
     
     def add_room(self, room_data):
         self.rooms.append(room_data)
