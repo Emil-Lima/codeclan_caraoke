13dd4d2fa7644ea75f9d74d817b92cfda9c6f6a5
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-08 13:21:13 +0100

    Add test to check if a room has a list of customers

--- tests/room_test.py
+++ tests/room_test.py
@@ -7,14 +7,19 @@ class TestRoom(unittest.TestCase):
     def setUp(self):
         guest1 = Guest("Emilio", 100)
         guest2 = Guest("John", 200)
+        self.list_customers = [guest1, guest2]
+
         song1 = Song("name of song 1", "rock")
         song2 = Song("name of song 2", "pop")
         self.list_of_songs = [song1, song2]
-        # self.room1 = Room("room1", ["song1", "song2", "song3"], [guest_1, guest_2])
-        self.room1 = Room("room1", self.list_of_songs)
+
+        self.room1 = Room("room1", self.list_of_songs, self.list_customers)
 
     def test_room_has_a_name(self):
         self.assertEqual("room1", self.room1.name)
     
     def test_room_has_list_of_songs(self):
         self.assertEqual(self.list_of_songs, self.room1.songs)
+
+    def test_room_has_list_of_customers(self):
+        self.assertEqual(self.list_customers, self.room1.customers)
