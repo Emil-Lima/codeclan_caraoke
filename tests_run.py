3fbc2aeade31463bad0da44e75c0b2ae10d460a5
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-08 11:44:57 +0100

    Uncomment import TestRoom in the main test file to allow the tests to run

--- tests_run.py
+++ tests_run.py
@@ -1,6 +1,6 @@
 import unittest
 # from tests.karaoke_test import TestKaraoke
-# from tests.room_test import TestRoom
+from tests.room_test import TestRoom
 from tests.guest_test import TestGuest
 
 if __name__ == "__main__":
