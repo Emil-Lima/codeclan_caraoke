662ceb09ade726ff5172692ba568b425171ed1bc
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-08 11:31:17 +0100

    Add tests for guest  - Comment out the other tests because they still don't have code to run

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -2,4 +2,11 @@ import unittest
 from src.guest import Guest
 
 class TestGuest(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.guest1 = Guest("John", 50)
+
+    def test_guest_has_name(self):
+        self.assertEqual("John", self.guest1.name)
+
+    def test_guest_has_money(self):
+        self.assertEqual(50, self.guest1.money)
--- tests_run.py
+++ tests_run.py
@@ -1,6 +1,7 @@
 import unittest
-from tests.karaoke_test import TestKaraoke
-from tests.room_test import TestRoom
+# from tests.karaoke_test import TestKaraoke
+# from tests.room_test import TestRoom
+from tests.guest_test import TestGuest
 
 if __name__ == "__main__":
     unittest.main()
