01c8f961fdfc6a07fd7bd779e2d2595d704c6dd3
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-08 13:30:31 +0100

    Update test to create an actual instance of the class Karaoke

--- tests/karaoke_test.py
+++ tests/karaoke_test.py
@@ -6,7 +6,7 @@ from src.song import Song
 
 class TestKaraoke(unittest.TestCase):
     def setUp(self):
-        self.karaoke1 = ("name of karaoke 1")
+        self.karaoke1 = Karaoke("name of karaoke 1")
     
     def test_karaoke_has_a_name(self):
         self.assertEqual("name of karaoke 1", self.karaoke1.name)
