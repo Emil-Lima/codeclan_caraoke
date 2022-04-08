a916cb75af4f1279e3e8dfde3421cb1b196ecd2b
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-08 16:10:01 +0100

    Add attribute money to class Room

--- src/room.py
+++ src/room.py
@@ -1,9 +1,10 @@
 class Room:
-    def __init__(self, input_name, input_songs, input_customers, input_maximum_capacity):
+    def __init__(self, input_name, input_songs, input_customers, input_maximum_capacity, input_money):
         self.name = input_name
         self.songs = input_songs
         self.customers = input_customers
         self.maximum_capacity = input_maximum_capacity
+        self.money = input_money
 
     def add_guest(self, guest_data):
         if len(self.customers) < self.maximum_capacity:
