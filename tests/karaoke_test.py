8f15fe6594e0fdc9795bd575016fe52ca6d54bbe
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-08 13:28:00 +0100

    Add test to check if an instance of class Karaoke has a name attribute

--- tests/karaoke_test.py
+++ tests/karaoke_test.py
@@ -1,5 +1,12 @@
 import unittest
 from src.karaoke import Karaoke
+from src.room import Room
+from src.guest import Guest
+from src.song import Song
 
 class TestKaraoke(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.karaoke1 = ("name of karaoke 1")
+    
+    def test_karaoke_has_a_name(self):
+        self.assertEqual("name of karaoke 1", self.karaoke1.name)
--- tests_run.py
+++ tests_run.py
@@ -1,5 +1,5 @@
 import unittest
-# from tests.karaoke_test import TestKaraoke
+from tests.karaoke_test import TestKaraoke
 from tests.room_test import TestRoom
 from tests.guest_test import TestGuest
 from tests.song_test import TestSong
