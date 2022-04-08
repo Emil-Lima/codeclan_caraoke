85638af2b81171b260a6d577349ac2474edb71a9
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-08 12:00:21 +0100

    Import TestSong to main tests file

--- tests_run.py
+++ tests_run.py
@@ -2,6 +2,7 @@ import unittest
 # from tests.karaoke_test import TestKaraoke
 from tests.room_test import TestRoom
 from tests.guest_test import TestGuest
+from tests.song_test import TestSong
 
 if __name__ == "__main__":
     unittest.main()
