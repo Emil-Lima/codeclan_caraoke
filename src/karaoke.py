c651fa418616bf8d024a0a291a333f769b3df2a6
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-08 13:57:27 +0100

    Add function to allow a karaoke to add a new room

--- src/karaoke.py
+++ src/karaoke.py
@@ -2,3 +2,6 @@ class Karaoke:
     def __init__(self, input_name, input_rooms):
         self.name = input_name
         self.rooms = input_rooms
+    
+    def add_room(self, room_data):
+        self.rooms.append(room_data)
