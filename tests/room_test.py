9eb2d486071115f88417b4737a1a17b2258cc3fc
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-08 11:42:35 +0100

    Modify room_test  - Import source code for Guest  - Create initial setUp for the tests

--- tests/room_test.py
+++ tests/room_test.py
@@ -1,5 +1,9 @@
 import unittest
 from src.room import Room
+from src.guest import Guest
 
 class TestRoom(unittest.TestCase):
-    pass
+    def setUp(self):
+        guest_1 = Guest("Emilio", 100)
+        guest_2 = Guest("John", 200)
+        self.room1 = Room(["song1", "song2", "song3"], [guest_1, guest_2])
