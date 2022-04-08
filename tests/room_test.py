9c0567aa97fd77861c5fb546a45e89eae5532e47
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-08 11:47:58 +0100

    Add test to check if a room has a name

--- tests/room_test.py
+++ tests/room_test.py
@@ -6,4 +6,8 @@ class TestRoom(unittest.TestCase):
     def setUp(self):
         guest_1 = Guest("Emilio", 100)
         guest_2 = Guest("John", 200)
-        self.room1 = Room(["song1", "song2", "song3"], [guest_1, guest_2])
+        self.room1 = Room("room1", ["song1", "song2", "song3"], [guest_1, guest_2])
+
+    def test_room_has_a_name(self):
+        self.assertEqual("room1", self.room1.name)
+    
