d9718ae1ceaccda673349a10a588750b94ba6617
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-08 12:08:38 +0100

    Add test to check if a room has an specific list of songs

--- tests/room_test.py
+++ tests/room_test.py
@@ -1,14 +1,19 @@
 import unittest
 from src.room import Room
 from src.guest import Guest
+from src.song import Song
 
 class TestRoom(unittest.TestCase):
     def setUp(self):
-        guest_1 = Guest("Emilio", 100)
-        guest_2 = Guest("John", 200)
+        guest1 = Guest("Emilio", 100)
+        guest2 = Guest("John", 200)
+        song1 = Song("name of song 1", "rock")
+        song2 = Song("name of song 2", "pop")
         # self.room1 = Room("room1", ["song1", "song2", "song3"], [guest_1, guest_2])
-        self.room1 = Room("room1")
+        self.room1 = Room("room1", [song1, song2])
 
     def test_room_has_a_name(self):
         self.assertEqual("room1", self.room1.name)
     
+    def test_room_has_list_of_songs(self):
+        self.assertEqual([song1, song2], self.room1.songs)
