e8d518beab1dfd7a4f17c513105a72b312163748
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-08 14:24:23 +0100

    Add attribute customers to class Karaoke

--- src/karaoke.py
+++ src/karaoke.py
@@ -1,7 +1,8 @@
 class Karaoke:
-    def __init__(self, input_name, input_rooms):
+    def __init__(self, input_name, input_rooms, input_guests):
         self.name = input_name
         self.rooms = input_rooms
+        self.guests = input_guests
     
     def add_room(self, room_data):
         self.rooms.append(room_data)
