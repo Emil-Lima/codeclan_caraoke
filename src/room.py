3f9684089d0ad6110a663942e9784ae87d14e151
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-08 13:22:02 +0100

    Add attribute customers to class Room

--- src/room.py
+++ src/room.py
@@ -1,4 +1,5 @@
 class Room:
-    def __init__(self, input_name, input_songs):
+    def __init__(self, input_name, input_songs, input_customers):
         self.name = input_name
         self.songs = input_songs
+        self.customers = input_customers
